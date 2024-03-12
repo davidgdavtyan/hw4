1b10bd6bce87139371118a2868605b29a6c24b00
David Davtyan <david.davtyan@kellogg.northwestern.edu>
2024-03-12 02:11:12 +0000

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,33 @@
 class SessionsController < ApplicationController
   def new
+    # render login form in sessions/new.html.erb
   end
 
   def create
+    # authenticate the user
+    # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email" => params["email"] })
+    # 2. if the user exists -> check if they know their password
+    if @user != nil
+  
+    # 3. if they know their password -> login is successful
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        flash["notice"] = "Welcome"
+        redirect_to "/places"
+    # 4. if the user doesn't exist or they don't know their password -> login fails
+      else 
+        flash["notice"] = "Nope"
+        redirect_to "/login"
+      end
+    else 
+      flash["notice"] = "Nope"
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    # logout the user
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
-  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,16 @@ class UsersController < ApplicationController
   def new
   end
 
+  def show
+    @user = User.find_by({ "id" => params["id"] })
+  end
+
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
     @user["password"] = params["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/places"
   end
 end
